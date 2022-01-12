e6d0d74709a7ab0ed048de7921a6e2b9e5795067
TureevS <sodnom_tureev@mail.ru>
2022-01-13 05:07:20 +1000

    update

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, jsonify
+from io import BytesIO
+from flask import Flask, request, jsonify, send_file
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 from datetime import timedelta
@@ -35,6 +36,14 @@ class Todolist(db.Model):
     username = db.Column(db.String(30), db.ForeignKey('users.username'))
 
 
+class Files(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    filenames = db.Column(db.String(300))
+    filesize = db.Column(db.String(30))
+    data = db.Column(db.LargeBinary)
+    username = db.Column(db.String(30), db.ForeignKey('users.username'))
+
+
 @app.route('/')
 def index():
     return {'TODO': 'list'}
@@ -46,12 +55,11 @@ def user():
         username = request.form['username']
         password = request.form['password']
         users = Users(username=username, password=password)
-
         db.session.add(users)
         db.session.commit()
         token = users.get_token()
         return {'token': token}
-    else:
+    elif request.method == 'GET':
         return {'Sign': 'Up'}
 
 
@@ -63,7 +71,7 @@ def authentication():
         users = Users.authentication(username, password)
         token = users.get_token()
         return {'token': token}
-    else:
+    elif request.method == 'GET':
         return {'Sign': 'In'}
 
 
@@ -97,15 +105,61 @@ def todo_update(id):
         item = Todolist.query.filter(Todolist.id == id, Todolist.username == username).first()
         text = request.form['doings']
         if not item:
-            return {'message': 'No item with this id'}, 400
+            resp = jsonify({'message': 'No item with this id'})
+            resp.status_code = 400
+            return resp
         item.doings = text
         db.session.commit()
         return {'status': 'success'}
     elif request.method == 'DELETE':
         item = Todolist.query.filter(Todolist.id == id, Todolist.username == username).first()
         if not item:
-            return {'message': 'No item with this id'}, 400
+            resp = jsonify({'message': 'No item with this id'})
+            resp.status_code = 400
+            return resp
         db.session.delete(item)
+        db.session.commit()
+        return {'status': 'success'}
+
+
+@app.route('/files', methods=['POST', 'GET'])
+@jwt_required()
+def files():
+    username = get_jwt_identity()
+    if request.method == 'POST':
+        file = request.files['file']
+        data = file.read()
+        size = str(len(data)) + ' byte'
+        new_file = Files(filenames=file.filename, filesize=size, data=data, username=username)
+        db.session.add(new_file)
+        db.session.commit()
+        return {'status': 'success'}
+    elif request.method == 'GET':
+        user_files = Files.query.filter(Files.username == username)
+        serialised = []
+        for file in user_files:
+            serialised.append({
+                'id': file.id,
+                'filename': file.filenames,
+                'size': file.filesize
+            })
+        return jsonify(serialised)
+
+
+@app.route('/files/<string:name>', methods=['GET', 'DELETE'])
+@jwt_required()
+def file_operation(name):
+    username = get_jwt_identity()
+    if request.method == 'GET':
+        file_data = Files.query.filter(Files.filenames == name, Files.username == username).first()
+        if not file_data:
+            return {'message': 'No file with this name'}
+        return send_file(BytesIO(file_data.data), attachment_filename=f"{name}", as_attachment=True)
+    elif request.method == 'DELETE':
+        file_data = Files.query.filter(Files.filenames == name, Files.username == username).first()
+        if not file_data:
+            return {'message': 'No file with this name'}
+        db.session.delete(file_data)
         db.session.commit()
         return {'status': 'success'}
 
