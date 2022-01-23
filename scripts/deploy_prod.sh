36da617e4fe97096997e4e9d6529931624efe253
TureevS <sodnom_tureev@mail.ru>
2022-01-23 15:15:23 +1000

    update

--- scripts/deploy_prod.sh
+++ scripts/deploy_prod.sh
@@ -1,5 +1,5 @@
-pip freeze > requirements.txt
+cd ../
 git add -A
 git commit -m "update"
 git push origin master
-ssh sodnom@128.199.122.96 'cd project-prod && source scripts/build.sh'
+ssh sodnom@128.199.122.96 'cd project-prod/scripts && sh build.sh'
